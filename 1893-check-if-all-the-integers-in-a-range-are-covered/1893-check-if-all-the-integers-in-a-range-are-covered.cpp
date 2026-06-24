4d2f44d54e750a41850ff835ad67fabae455e4ff
SatyaDayakar <satyadayakar5@gmail.com>
2026-06-25 02:05:00 +0530

    Time: 0 ms (100%), Space: 12.1 MB (54.47%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        unordered_map<int, bool> covered;
-
-        for (auto range : ranges) {
-            int start = range[0];
-            int end = range[1];
-
-            for (int num = start; num <= end; num++) {
-                covered[num] = true;
-            }
+        vector<int> arr(52, 0);
+        for(auto& i: ranges)
+        {
+            arr[i[0]]++;
+            arr[i[1] + 1]--;
         }
-
-
-        for (int num = left; num <= right; num++) {
-            if (covered[num] == false) {
-                return false;
-            }
+        for(int i = 1; i < 51; i++)
+        {
+            arr[i] += arr[i-1];
         }
-
+        for(int i = left; i <= right; i++)
+        {
+            if(arr[i] == 0) return false;
+        }
         return true;
     }
 };
